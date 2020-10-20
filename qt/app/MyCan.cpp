cee3f37744d21780e2556fcae23e649337a4316a
zhangran <28281539@qq.com>
2020-10-20 17:04:05 +0800

    sync

---
+++ qt/app/MyCan.cpp
@@ -0,0 +1,6 @@
+#include "YyCan.h"
+
+MyCan::MyCan()
+{
+
+}
---
+++ qt/app/MyCan.h
@@ -0,0 +1,13 @@
+#ifndef MYCAN_H
+#define MYCAN_H
+
+#include <QObject>
+
+class MyCan
+{
+    Q_OBJECT
+public:
+    MyCan();
+};
+
+#endif // MYCAN_H
